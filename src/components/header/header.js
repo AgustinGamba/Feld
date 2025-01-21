40ca618185b8edf72539503287d3f761b247571e
Agustin Gamba <agustin.o.gamba@gmail.com>
2025-01-21 19:17:36 -0300

    Add components folder

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,10 +1,16 @@
-import { Container } from '@mui/material'
-;
+import { Container, SvgIcon } from '@mui/material'
+// import MenuIcon from '@mui/icons-material/Menu';
+
+import feldLogo from '../../assets/feldLogoColor.svg'
+
 import styles from './header.module.css';
 
 function Header() {
   return (
     <Container className={styles.headerContainer}>
+      <Container>
+        <SvgIcon className={styles.HeaderLogo}>{feldLogo}</SvgIcon>
+      </Container>
       <div className={styles.headerText}>
         We boost brands with creativity and digital solutions
       </div>
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { ThemeProvider } from '@mui/material';
 import { StyledEngineProvider } from '@mui/material/styles';
 
 import theme from './theme';
-import App from './app/app';
+import App from './components/app/app';
 import './index.module.css';
 import reportWebVitals from './reportWebVitals';
 
