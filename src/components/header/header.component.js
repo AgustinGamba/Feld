1a66a796701622bc80883115e96af2c42e70876a
Agustin Gamba <agustin.o.gamba@gmail.com>
2025-01-22 00:26:46 -0300

    Update header to use flex in js

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -1,39 +1,48 @@
 import { Container, Box, Link, Typography } from '@mui/material'
 import MenuIcon from '@mui/icons-material/Menu';
 import NorthEastIcon from '@mui/icons-material/NorthEast';
+import classNames from 'classnames';
 
 import feldLogo from '../../assets/feldLogoColor.svg'
-
 import styles from './header.module.css';
 
 function Header() {
+  const digitalSolutionsClass = classNames(styles.headerText, styles.boldText)
+
   return (
     <Container className={styles.headerContainer}>
-      <Box className={styles.headerBox}>
+      <Box className={styles.headerBox} display="flex" justifyContent="space-between">
         {/* TODO: Resize logo */}
         <img className={styles.headerLogo} src={feldLogo} />
         {/* TODO: Resize icon, add functionality */}
         <MenuIcon />
       </Box>
-      <Box className={styles.headerTextBox}>
-        <Box>
+      {/* TODO: Define space between header bar and this */}
+      <Box className={styles.headerTextBox} display="flex" gap={5} alignItems="center" flexDirection="column">
+        <Box display="flex" flexDirection="column">
           <Typography variant="body1" className={styles.headerText}>
             We boost brands
           </Typography>
           <Typography variant="body1" className={styles.headerText}>
             with creativity
           </Typography>
-          <Typography variant="body1" className={styles.headerText}>
-            and digital solutions
-          </Typography>
-          <Box>
-            {/* TODO: Fix color and position, too high */}
-            <NorthEastIcon />
-            <Link href="#" className={styles.headerLink}>See our work</Link>
+          <Box display="flex" flexDirection="row">
+            <Typography variant="body1" className={styles.headerText}>
+              and&nbsp;
+            </Typography>
+            <Typography variant="body1" className={digitalSolutionsClass}>
+              digital solutions
+            </Typography>
           </Box>
         </Box>
+        <Box display="flex" flexDirection="row">
+          {/* TODO: Fix color and position, too high */}
+          <NorthEastIcon />
+          <Link href="#" className={styles.headerLink}>See our work</Link>
+        </Box>
+      {/* TODO: Define space between header text and header end */}
       </Box>
-    </Container>
+    </Container >
   );
 }
 
--- src/theme.js
+++ src/theme.js
@@ -6,7 +6,8 @@ const theme = createTheme({
         MuiContainer: {
             defaultProps: {
                 disableGutters: true,
-                maxWidth: 100
+                maxWidth: 100,
+                spacing: 8
             },
         },
     },
