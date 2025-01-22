2e9ef8268be0fab42fa9ac4501ab517fb13484bf
Agustin Gamba <agustin.o.gamba@gmail.com>
2025-01-21 23:48:06 -0300

    Add header data

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -1,5 +1,6 @@
-import { Container, SvgIcon } from '@mui/material'
-// import MenuIcon from '@mui/icons-material/Menu';
+import { Container, Box, Link } from '@mui/material'
+import MenuIcon from '@mui/icons-material/Menu';
+import NorthEastIcon from '@mui/icons-material/NorthEast';
 
 import feldLogo from '../../assets/feldLogoColor.svg'
 
@@ -8,15 +9,28 @@ import styles from './header.module.css';
 function Header() {
   return (
     <Container className={styles.headerContainer}>
-      <Container>
-        <SvgIcon className={styles.HeaderLogo}>{feldLogo}</SvgIcon>
-      </Container>
-      <div className={styles.headerText}>
-        We boost brands with creativity and digital solutions
-      </div>
-      <div>
-        See our work
-      </div>
+      <Box className={styles.headerBox}>
+        {/* TODO: Resize logo */}
+        <img className={styles.headerLogo} src={feldLogo} />
+        {/* TODO: Resize icon */}
+        <MenuIcon />
+      </Box>
+      <Box className={styles.headerTextBox}>
+        <div className={styles.headerText}>
+          We boost brands
+        </div>
+        <div className={styles.headerText}>
+          with creativity
+        </div>
+        <div className={styles.headerText}>
+          and digital solutions
+        </div>
+        <Box>
+          {/* TODO: Fix color and position, too high */}
+          <NorthEastIcon />
+          <Link href="#" className={styles.headerLink}>See our work</Link>
+        </Box>
+      </Box>
     </Container>
   );
 }
