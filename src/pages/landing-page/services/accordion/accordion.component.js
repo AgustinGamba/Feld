24942d9b361d457d7c5e412e266c0845e6a0e8f6
Agustin Gamba <agustin.o.gamba@gmail.com>
2025-02-07 02:14:49 -0300

    Finish accordion style

--- src/pages/landing-page/services/accordion/accordion.component.js
+++ src/pages/landing-page/services/accordion/accordion.component.js
@@ -8,18 +8,26 @@ import styles from './accordion.module.css';
 function FeldAccordion({ service, position }) {
   return (
     <Container>
-      < Accordion square className={styles.accordionContainer} >
+      <Accordion square className={styles.accordionContainer} >
         <AccordionSummary
-          // TODO: Change arrow color
-          expandIcon={<NorthEast />}
           aria-controls={`accordion-${position}-content`}
           id={`panel-${position}-header`}
+          sx={{
+            // TODO: Move to stylesheet
+            '& .MuiAccordionSummary-content': {
+              paddingBottom: "28px",
+              borderBottom: "solid 1px",
+              margin: "20px 10px"
+            },
+          }}
         >
           <Box display="flex" gap={1} alignItems="center">
             <Pill position={position} />
             <Typography variant='h5' >
               {service.name}
             </Typography>
+            {/* TODO: Change arrow color */}
+            <NorthEast />
           </Box>
         </AccordionSummary>
         <AccordionDetails>
