21e14018bfa12290f16ded3376e90f18a63a89fd
Agustin Gamba <agustin.o.gamba@gmail.com>
2025-02-07 01:51:58 -0300

    Fix white line over accordion

--- src/pages/landing-page/services/accordion/accordion.component.js
+++ src/pages/landing-page/services/accordion/accordion.component.js
@@ -1,4 +1,4 @@
-import { Typography, Accordion, AccordionSummary, AccordionDetails, Box } from '@mui/material';
+import { Typography, Accordion, AccordionSummary, AccordionDetails, Box, Container } from '@mui/material';
 import { NorthEast } from '@mui/icons-material';
 
 import Pill from "../pill/pill.component"
@@ -7,26 +7,28 @@ import styles from './accordion.module.css';
 
 function FeldAccordion({ service, position }) {
   return (
-    < Accordion square className={styles.accordionContainer} >
-      <AccordionSummary
-        // TODO: Change arrow color
-        expandIcon={<NorthEast />}
-        aria-controls={`accordion-${position}-content`}
-        id={`panel-${position}-header`}
-      >
-        <Box display="flex" gap={1} alignItems="center">
-          <Pill position={position} />
-          <Typography variant='h5' >
-            {service.name}
+    <Container>
+      < Accordion square className={styles.accordionContainer} >
+        <AccordionSummary
+          // TODO: Change arrow color
+          expandIcon={<NorthEast />}
+          aria-controls={`accordion-${position}-content`}
+          id={`panel-${position}-header`}
+        >
+          <Box display="flex" gap={1} alignItems="center">
+            <Pill position={position} />
+            <Typography variant='h5' >
+              {service.name}
+            </Typography>
+          </Box>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography className={styles.servicesDescriptionText}>
+            {service.description}
           </Typography>
-        </Box>
-      </AccordionSummary>
-      <AccordionDetails>
-        <Typography className={styles.servicesDescriptionText}>
-          {service.description}
-        </Typography>
-      </AccordionDetails>
-    </Accordion >
+        </AccordionDetails>
+      </Accordion >
+    </Container>
   )
 }
 
