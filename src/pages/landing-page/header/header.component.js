49d1c0caf3e887f4a6c89fd7307d2bd46565fd10
Agustin Gamba <agustin.o.gamba@gmail.com>
2025-02-16 00:18:54 -0300

    Add header text animation

--- src/pages/landing-page/header/header.component.js
+++ src/pages/landing-page/header/header.component.js
@@ -1,33 +1,22 @@
 import { Box, Link, Typography } from '@mui/material'
 import { Menu, NorthEast } from '@mui/icons-material';
-import clsx from 'clsx';
 
 import PaddedContainer from '../../../components/padded-container/paddedContainer.component';
+import Headline from "./headline/headline.component";
 import feldLogo from '../../../assets/feldLogoColor.svg'
 import styles from './header.module.css';
 
 function Header() {
-  const digitalSolutionsClass = clsx(styles.headerText, styles.headerBoldText)
 
   return (
     <PaddedContainer className={styles.headerContainer}>
       <Box display="flex" flexDirection="column" gap={9}>
         <Box display="flex" justifyContent="space-between">
           <img alt='Feld Logo' className={styles.headerLogo} src={feldLogo} />
-          {/* TODO: add functionality */}
           <Menu className={styles.headerMenuIcon} />
         </Box>
         <Box className={styles.headerTextBox} display="flex" gap={4} alignSelf="center" flexDirection="column" textAlign="center">
-          <Box display="flex" flexDirection="column" alignContent="center" alignItems="center" alignSelf="center" justifyContent="center">
-            <Typography variant="h2" className={styles.headerText}>
-              We boost brands
-              with creativity
-              and&nbsp;
-            </Typography>
-            <Typography variant="h2" className={digitalSolutionsClass}>
-              digital solutions.
-            </Typography>
-          </Box>
+          <Headline />
           <Box display="flex" flexDirection="row" alignItems="center" justifyContent="center">
             <NorthEast className={styles.headerHyperlink} />
             <Link href="#" className={styles.headerHyperlink}>
---
+++ src/pages/landing-page/header/headline/headline.component.js
@@ -0,0 +1,68 @@
+import { useEffect, useRef } from "react";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import { Box, Typography } from "@mui/material";
+
+import arrow from "../../../../assets/arrow.svg";
+import asterisk from "../../../../assets/asterisk.svg";
+
+import styles from "./headline.module.css";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const MainText = () => {
+  const beforeArrowTextRef = useRef(null);
+  const arrowRef = useRef(null);
+  const afterArrowTextRef = useRef(null);
+  const asteriskRef = useRef(null);
+  const underlineRef = useRef(null);
+
+  useEffect(() => {
+    const animation = gsap.timeline({ repeat: -1, yoyo: true, repeatDelay: 1 });
+
+    animation.set([arrowRef.current, asteriskRef.current], { opacity: 0, scale: 0.5 })
+      .fromTo(beforeArrowTextRef.current, { x: 20 }, { x: -10, duration: 0.5, ease: "power2.out" }, 0)
+      .fromTo(afterArrowTextRef.current, { x: -15 }, { x: 10, duration: 0.5, ease: "power2.out" }, 0)
+      .to(arrowRef.current, { opacity: 1, scale: 1, duration: 0.5, ease: "back.out(1.7)" }, "-=0.3")
+      .to(
+        asteriskRef.current,
+        { opacity: 1, scale: 1, rotate: 360, duration: 0.8, ease: "back.out(1.7)", transformOrigin: "center" },
+        "+=0.2"
+      )
+      .to(underlineRef.current, { opacity: 1, scaleX: 1, duration: 0.5, ease: "power2.out" }, "+=0.3");
+  }, []);
+
+  return (
+    <Box>
+      <Box display="flex" alignItems="center" justifyContent="center">
+        <Typography ref={beforeArrowTextRef} variant="h2" className={styles.headlineText}>
+          We
+        </Typography>
+        <img alt='Arrow' src={arrow} ref={arrowRef} className={styles.headlineArrow} />
+        <Typography ref={afterArrowTextRef} variant="h2" className={styles.headlineText}>
+          boost brands
+        </Typography>
+      </Box>
+      <Box display="flex" alignItems="baseline" justifyContent="center">
+        <Typography variant="h2" className={styles.headlineText}>
+          with creativity&nbsp;
+        </Typography>
+        <img ref={asteriskRef} alt="Asterisk" src={asterisk} className={styles.headlineAsterisk} />
+      </Box>
+      <Box display="flex" justifyContent="center">
+        <Typography variant="h2" className={styles.headlineText}>and&nbsp;</Typography>
+        <Box display="flex" flexDirection="column" gap={1}>
+          <Typography variant="h2" className={styles.headlineDarkerText}>
+            digital solutions
+          </Typography>
+          <Box height="3px"
+            ref={underlineRef}
+            className={styles.headlineTextUnderline}
+          />
+        </Box>
+      </Box>
+    </Box >
+  );
+};
+
+export default MainText;
