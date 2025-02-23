5fc48ec8e9bdfeec9d27388e97476068631e84c3
Agustin Gamba <agustin.o.gamba@gmail.com>
2025-02-23 19:09:23 -0300

    Update app theme sizes on mobile

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from '@mui/material';
-import { StyledEngineProvider } from '@mui/material/styles';
 
 import theme from './theme';
 import App from './app.component';
@@ -10,13 +9,11 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <StyledEngineProvider injectFirst>
-    <React.StrictMode>
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
-    </React.StrictMode>
-  </StyledEngineProvider>
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/theme.js
+++ src/theme.js
@@ -1,90 +1,88 @@
 
 import { createTheme } from '@mui/material';
 
-const theme = createTheme({
-    components: {
-        MuiContainer: {
-            defaultProps: {
-                disableGutters: true,
-                spacing: 8,
-                maxWidth: false,
-            },
-        },
-        MuiTextField: {
-            defaultProps: {
-                variant: "standard",
-            },
-            styleOverrides: {
-                root: {
-                    "& .MuiInputBase-input": {
-                        color: "#FFFFFF",
-                        fontSize: "16px",
-                        // TODO: Adjust width based on screen size
-                        minWidth: "180px"
-                    },
-                    "& .MuiInputLabel-root": {
-                        color: "#FFFFFF",
-                        fontSize: "18px",
-                    },
-                    "& .MuiInputLabel-root.Mui-focused": {
-                        color: "#FFFFFF",
-                    },
-                    "& .MuiInputLabel-root.MuiInputLabel-shrink": {
-                        fontSize: "14px",
-                    },
-                    "& .MuiInput-underline:before": {
-                        borderBottomColor: "#FFFFFF",
-                        opacity: 0.7,
-                    },
-                    "& .MuiInput-underline:hover:before": {
-                        borderBottomColor: "#FFFFFF !important",
-                        opacity: 1,
-                    },
-                    "& .MuiInput-underline:after": {
-                        borderBottomColor: "#FFFFFF",
-                    },
-                },
-            },
-        },
+const theme = createTheme();
+
+const updatedTheme = createTheme(theme, {
+  components: {
+    MuiContainer: {
+      defaultProps: {
+        disableGutters: true,
+        spacing: 8,
+        maxWidth: false,
+      },
     },
-    typography: {
-        fontFamily: 'Manrope',
-        fontWeightLight: 400,
-        h1: {
-            fontSize: '64px',
+    MuiTextField: {
+      defaultProps: {
+        variant: "standard",
+      },
+      styleOverrides: {
+        root: {
+          "& .MuiInputBase-input": {
+            color: "#FFFFFF",
+            fontSize: "16px",
+            // TODO: Adjust width based on screen size
+            minWidth: "180px"
+          },
+          "& .MuiInputLabel-root": {
+            color: "#FFFFFF",
+            fontSize: "18px",
+          },
+          "& .MuiInputLabel-root.Mui-focused": {
+            color: "#FFFFFF",
+          },
+          "& .MuiInputLabel-root.MuiInputLabel-shrink": {
+            fontSize: "14px",
+          },
+          "& .MuiInput-underline:before": {
+            borderBottomColor: "#FFFFFF",
+            opacity: 0.7,
+          },
+          "& .MuiInput-underline:hover:before": {
+            borderBottomColor: "#FFFFFF !important",
+            opacity: 1,
+          },
+          "& .MuiInput-underline:after": {
+            borderBottomColor: "#FFFFFF",
+          },
         },
-        h2: {
-            fontSize: '60px',
-        },
-        h3: {
-            fontSize: '56px',
-        },
-        h4: {
-            fontSize: '36px',
-        },
-        h5: {
-            fontSize: '20px',
-        },
-        body1: {
-            fontSize: '20px',
-        },
-        body2: {
-            fontSize: '16px',
-        },
-        body3: {
-            fontSize: '14px',
-        }
+      },
     },
-    breakpoints: {
-        // TODO: use or remove
-        values: {
-            xs: 0,
-            sm: 600,
-            md: 900,
-            lg: 1200,
-            xl: 1536,
-        },
+  },
+  typography: {
+    fontFamily: 'Manrope',
+    fontWeightLight: 400,
+    h1: {
+      fontSize: '64px',
+      '@media (max-width:600px)': {
+        fontSize: '36px',
+      },
     },
+    h2: {
+      fontSize: '60px',
+      [theme.breakpoints.down('sm')]: {
+        fontSize: '24px',
+      },
+    },
+    h3: {
+      fontSize: '56px',
+    },
+    h4: {
+      fontSize: '36px',
+    },
+    body1: {
+      fontSize: '20px',
+      [theme.breakpoints.down('sm')]: {
+        fontSize: '18px',
+      },
+    },
+    body2: {
+      fontSize: '16px',
+    },
+    body3: {
+      fontSize: '14px',
+    }
+  }
 });
 
-export default theme;
+export default updatedTheme;
