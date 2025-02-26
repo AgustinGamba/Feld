9023fdf657a5cf29ebf5116c9d8d4f1882b4d2a6
Agustin Gamba <agustin.o.gamba@gmail.com>
2025-02-26 20:23:29 -0300

    Update headline underline to show from left to right

--- src/pages/landing-page/header/headline/headline.component.js
+++ src/pages/landing-page/header/headline/headline.component.js
@@ -20,7 +20,10 @@ const MainText = () => {
   useEffect(() => {
     const animation = gsap.timeline({ repeat: -1, yoyo: true, repeatDelay: 1 });
 
-    animation.set([arrowRef.current, asteriskRef.current, underlineRef.current], { opacity: 0, scale: 0.5, scaleX: 0 })
+    animation.set(
+      [arrowRef.current, asteriskRef.current, underlineRef.current],
+      { opacity: 0, scale: 0.5, scaleX: 0 }
+    )
       .fromTo(afterArrowTextRef.current, { x: -25 }, { x: 20, duration: 0.5, ease: "power2.out" }, 0)
       .to(arrowRef.current, { x: 10, opacity: 1, scale: 1, duration: 0.5, ease: "back.out(1.7)" }, "-=0.3")
       .to(
@@ -28,7 +31,12 @@ const MainText = () => {
         { opacity: 1, scale: 1, rotate: 360, duration: 0.8, ease: "back.out(1.7)", transformOrigin: "center" },
         "+=0.2"
       )
-      .to(underlineRef.current, { opacity: 1, scaleX: 1, duration: 0.5, ease: "power2.out" }, "+=0.3");
+      .to(
+        underlineRef.current,
+        { opacity: 1, scaleX: 1, duration: 0.5, ease: "power2.out", transformOrigin: "left" },
+        "+=0.3"
+      );
+
   }, []);
 
   return (
