780c7445a7104fb6d9cc76e64b83b1609dd464ad
Agustin Gamba <agustin.o.gamba@gmail.com>
2025-02-28 18:21:02 -0300

    Update bluebox to adapt to mobile size

--- src/pages/landing-page/contact-us/blueBox/blueBox.component.js
+++ src/pages/landing-page/contact-us/blueBox/blueBox.component.js
@@ -1,14 +1,22 @@
 import { Box, Typography } from '@mui/material';
 
 import PaddedContainer from '../../../../components/padded-container/paddedContainer.component';
+import { useBreakpoint } from "../../../../components/breakpoint-provider/breakpointProvider.component";
 import Form from "./form/form.component";
 
 import styles from './blueBox.module.css';
 
 function BlueBox() {
+  const { isSmallerThanOrEqual } = useBreakpoint();
+
   return (
     <PaddedContainer className={styles.blueBoxContainer}>
-      <Box display="flex" flexDirection="row">
+      <Box display="flex"
+        sx={{
+          padding: isSmallerThanOrEqual("sm") ? "50px 0" : 0,
+          flexDirection: isSmallerThanOrEqual("sm") ? "column" : "row",
+          gap: isSmallerThanOrEqual("sm") ? 3 : 0,
+        }}>
         <Box display="flex" className={styles.blueBoxHalfContainer}>
           <Typography textAlign='center' variant="h1" className={styles.blueBoxWhiteText}>
             Contact us.
--- src/pages/landing-page/contact-us/blueBox/form/form.component.js
+++ src/pages/landing-page/contact-us/blueBox/form/form.component.js
@@ -2,20 +2,32 @@ import { Box, Container, TextField, Typography } from '@mui/material';
 import clsx from 'clsx';
 
 import { NorthEast } from '@mui/icons-material';
+import { useBreakpoint } from "../../../../../components/breakpoint-provider/breakpointProvider.component";
+
 import styles from './form.module.css';
 
 function Form() {
   const arrowIconClass = clsx(styles.contactUsWhiteText, styles.contactUsArrowIcon)
+  const { isSmallerThanOrEqual } = useBreakpoint();
 
   return (
-    <Box display="flex" flexDirection="column" gap={2} width="fit-content">
+    <Box display="flex" gap={2} flexDirection="column" 
+    sx={{
+      minWidth: isSmallerThanOrEqual("sm") ? "100%" : "fit-content",
+    }}>
       {/* TODO: Add a form */}
       <Box display="flex" flexDirection="column" gap={2}>
-        <Box display="flex" flexDirection="row" gap={3}>
+        <Box display="flex" gap={3}
+          sx={{
+            flexDirection: isSmallerThanOrEqual("sm") ? "column" : "row",
+          }}>
           <TextField id="full-name" label="Full name" />
           <TextField id="company" label="Company" />
         </Box>
-        <Box display="flex" flexDirection="row" gap={3}>
+        <Box display="flex" flexDirection="row" gap={3}
+          sx={{
+            flexDirection: isSmallerThanOrEqual("sm") ? "column" : "row",
+          }}>
           <TextField id="email" label="Email address" />
           <TextField id="service" label="Service" />
         </Box>
